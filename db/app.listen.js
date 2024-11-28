66c0ed54c12bc2629f691403afc5644b2bf40255
LeeDux <lee.duxbury94@yahoo.com>
2024-11-28 15:57:38 +0000

    updated listen.js, connections.js and package.json for superseed

--- db/app.listen.js
+++ db/app.listen.js
@@ -1,5 +1,10 @@
-const app = require("../db/app");
+/* const app = require("../db/app");
 
 app.listen(8000, () => {
   console.log("Server is listening on port 8000...");
-});
+}); */
+
+const app = require("./app");
+const { PORT = 8000 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
