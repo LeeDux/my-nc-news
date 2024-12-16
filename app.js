a9b0abaf3406d85203481c7a2b0c5286a25ea4dc
LeeDux <lee.duxbury94@yahoo.com>
2024-12-16 10:03:29 +0000

    allow cross origin resource sharing

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const cors = require("cors");
+app.use(cors());
+
+const {
+  getApi,
+  getTopics,
+  getArticleById,
+  getAllArticles,
+  getComments,
+  postComment,
+  patchVotes,
+  deleteCommentByiD,
+  getUsers,
+} = require("./db/seeds/api.controller");
+
+console.log("I am in app");
+
+app.get("/api", getApi);
+
+app.get("/api/topics", getTopics);
+
+app.get("/api/articles", getAllArticles);
+
+app.get("/api/articles/:article_id", getArticleById);
+
+app.get("/api/articles/:article_id/comments", getComments);
+
+app.post("/api/articles/:article_id/comments", postComment);
+
+app.patch("/api/articles/:article_id", patchVotes);
+
+app.delete("/api/comments/:comment_id", deleteCommentByiD);
+
+app.get("/api/users", getUsers);
+
+app.all("*", (req, res) => {
+  res.status(404).send({ msg: "Not Found" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  if (err.code === "22P02") {
+    return res.status(400).send({ msg: "Bad Request" });
+  }
+
+  if (err.status && err.msg) {
+    return res.status(err.status).send({ msg: err.msg });
+  }
+
+  if (err.status === 400) {
+    return res.status(400).send({ msg: "Bad Request" });
+  }
+
+  if (err.status === 404) {
+    return res.status(404).send({ msg: "Not Found" });
+  }
+
+  return res.status(500).send({ msg: "Internal Server Error" });
+});
+
+module.exports = app;
